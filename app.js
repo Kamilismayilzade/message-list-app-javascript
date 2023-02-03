a5711b85e608d4b22cf539064d6bd01637b15b0c
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-03 15:12:34 +0100

    Final -  local storage fix

--- app.js
+++ app.js
@@ -1,89 +1,129 @@
-window.addEventListener('load', function() {
+    window.addEventListener('load', function() {
 
-    const form = document.querySelector('.form');
-    const allMessages = document.querySelector('#messages');
-    const messageInput = document.querySelector('.message-input');
-
-    const addMessageBtn = document.querySelector('.message-submit');
-    const toastContainer = document.getElementById("toast-container");
-
-    // Get messages from local storage or set an empty array
-    // let messages = localStorage.getItem("messages") ? JSON.parse(localStorage.getItem("messages")) : [];
-
-    let messages = [];
-
-    const storedMessages = localStorage.getItem("messages");
-
-    if (storedMessages) {
-        try {
-            messages = JSON.parse(storedMessages);
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
-
+        const form = document.querySelector('.form');
+        const allMessages = document.querySelector('#messages');
+        const messageInput = document.querySelector('.message-input');
+        const toastContainer = document.getElementById("toast-container");
     
-    form.addEventListener('submit', function(e) {
+        // Get messages from local storage or set an empty array
+        let messages = [];
+    
+        const storedMessages = localStorage.getItem("messages");
+    
+        if (storedMessages) {
 
-        e.preventDefault();
+            try {
 
-        if (messageInput.value === '') return;
+                messages = JSON.parse(storedMessages) || [];
 
-        messages.push(messageInput.value);
-        messageInput.value = '';
+            } catch (err) {
 
-        localStorage.setItem("messages", JSON.stringify(messages));
+                console.error(err);
+                alert("Error: local storage data is corrupted. Please reload the page.");
 
-        const message = messages[messages.length - 1];
+            }
 
-        const newMessage = document.createElement('div');
-        newMessage.classList.add('message');
+        }
+    
+        // Render messages from local storage to UI
 
-        newMessage.innerHTML = `
-          <div class="content">
-            <input type="text" class="text" value="${message}" readonly > 
-          </div>
+        messages.forEach(message => {
 
-          <div class="action">
-            <button class="delete"> <i class="fa-solid fa-xmark"></i> </button>
-          </div>
-        `;
+            const newMessage = document.createElement('div');
+            newMessage.classList.add('message');
+    
+            newMessage.innerHTML = `
+            <div class="content">
+                <input type="text" class="text" value="${message}" readonly > 
+            </div>
+    
+            <div class="action">
+                <button class="delete"> <i class="fa-solid fa-xmark"></i> </button>
+            </div>
+            `;
+    
+            allMessages.appendChild(newMessage);
+    
+            const deleteBtn = newMessage.querySelector('.delete');
+    
+            deleteBtn.addEventListener('click', function() {
+    
+                newMessage.remove();
+                removeToast();
+    
+                // Remove the deleted message from local storage
+                messages = messages.filter(m => m !== message);
 
-        allMessages.appendChild(newMessage);
-        showToast(message);
-
-        const deleteBtn = newMessage.querySelector('.delete');
-
-        deleteBtn.addEventListener('click', function() {
-
-            newMessage.remove();
-            removeToast();
+                localStorage.setItem("messages", JSON.stringify(messages));
+                
+            });
 
         });
-    })
+    
+        form.addEventListener('submit', function(e) {
+    
+            e.preventDefault();
+    
+            if (messageInput.value === '') return;
+    
+            messages.push(messageInput.value);
+            messageInput.value = '';
+    
+            localStorage.setItem("messages", JSON.stringify(messages));
+    
+            const message = messages[messages.length - 1];
+    
+            const newMessage = document.createElement('div');
+            newMessage.classList.add('message');
+    
+            newMessage.innerHTML = `
+            <div class="content">
+                <input type="text" class="text" value="${message}" readonly > 
+            </div>
+    
+            <div class="action">
+                <button class="delete"> <i class="fa-solid fa-xmark"></i> </button>
+            </div>
+            `;
+    
+            allMessages.appendChild(newMessage);
+            showToast(message);
+    
+            const deleteBtn = newMessage.querySelector('.delete');
+    
+            deleteBtn.addEventListener('click', function() {
+    
+                newMessage.remove();
+                removeToast();
+    
+                // Remove the deleted message from local storage
+                messages = messages.filter(m => m !== message);
 
-    function showToast(message) {
+                localStorage.setItem("messages", JSON.stringify(messages));
+            });
+        })
 
-        // remove existing toast element
-        toastContainer.innerHTML = '';
-      
-        const toast = document.createElement('div');
-        toast.classList.add('toast');
-        toast.innerHTML = `<span><i class="fa-sharp fa-solid fa-circle-info"></i></span> ${message}`;
-        toastContainer.style.display = 'block';
-        toastContainer.appendChild(toast);
-      
-        setTimeout(() => {
-            removeToast();
-        }, 5000);
+        const showToast = function (message) {
 
-    }
-
-    function removeToast() {
-
-        toastContainer.style.display = 'none';
-        toastContainer.innerHTML = '';
+            // remove existing toast element
+            toastContainer.innerHTML = '';
         
-    }
-});
+            const toast = document.createElement('div');
+            toast.classList.add('toast');
+            toast.innerHTML = `<span><i class="fa-sharp fa-solid fa-circle-info"></i></span> ${message}`;
+            toastContainer.style.display = 'block';
+            toastContainer.appendChild(toast);
+        
+            setTimeout(() => {
+                removeToast();
+            }, 5000);
+
+        }
+
+        const removeToast = function() {
+
+            toastContainer.style.display = 'none';
+            toastContainer.innerHTML = '';
+            
+        }
+    });
